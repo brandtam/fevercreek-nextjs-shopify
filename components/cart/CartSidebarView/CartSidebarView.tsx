4cce29f3454bebe8950eb29b48a437e5b27c7df1
Brandt Milczewski <brandt@shopdevs.com>
2021-08-03 19:55:46 -0700

    minor update

--- components/cart/CartSidebarView/CartSidebarView.tsx
+++ components/cart/CartSidebarView/CartSidebarView.tsx
@@ -55,7 +55,7 @@ const CartSidebarView: FC = () => {
           <Bag />
           Your cart is empty
           <Text>
-            Biscuit oat cake wafer icing ice cream tiramisu pudding cupcake.
+            Please find something you like!
           </Text>
         </>
       ) : (
